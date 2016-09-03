61eb04ae84672bd77a1d78d683f33b4a50bc9d54
kfrei <freiderikosk@hotmail.com>
2016-09-03 09:22:53 +0300

    mod4

---
+++ module4-solution/js/script.js
@@ -0,0 +1,40 @@
+
+var x;
+
+console.log(x);
+
+if (x == undefined){
+	console.log(" x is undefined da friend !");
+}
+
+x=5;
+if (x == undefined){
+	console.log(" x is undefined  my friend !");
+}
+else {
+	console.log(" x is " + x);
+}
+/*
+var a = function () {
+	var msg="inside a";
+	console.log("a: msg " + msg);
+	
+	function c () {
+		console.log("c: msg " + msg);
+	}
+	console.log("xA1=" + x);
+	b(); c();
+	console.log("xA2=" + x);
+	
+}
+
+function b () {
+	console.log("b: msg " + msg);
+	x++;
+	console.log("xB=" + x);
+}
+
+a();
+console.log("xGL=" + x);
+
+*/
