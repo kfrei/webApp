3cd18fbeaae9d982f94e55703d6eaf478e9d0cb6
kfrei <freiderikosk@hotmail.com>
2016-11-04 02:01:49 +0200

    .

--- Schol.io/js/results-app.js
+++ Schol.io/js/results-app.js
@@ -12,10 +12,26 @@ angular.module("resultsApp",[])
 
         $scope.sortType     = 'ratingStar'; // set the default sort type
         $scope.sortReverse  = false;  // set the default sort order
-        //$scope.cityFilter ='';
-        //$scope.categoryFilter ='Εκπαιδευτικοί Φορείς';
+
+        $scope.scholars=0;
+        $scope.reviews=0;
+        $scope.categoryFilter="null";
+
+        $scope.checkBoxFilter = function(item){
+            filtered = [];
+            if(item.scholarships >=  $scope.scholars && item.ratingCounter >=  $scope.reviews
+                //&& ( item.city == $scope.cityFilter || $scope.cityFilter==null )
+                && (item.category == $scope.categoryFilter || $scope.categoryFilter =="null") ) {
+                filtered.push(item);
+                return filtered;
+
+            }
+
+        };
 
     });
+
+
 
 
 
