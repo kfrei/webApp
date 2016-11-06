63dfc720fa185c554c19854720cdbc4e5a211bf9
kfrei <freiderikosk@hotmail.com>
2016-11-06 23:02:36 +0200

    .

--- Schol.io/js/results-app.js
+++ Schol.io/js/results-app.js
@@ -26,18 +26,30 @@ angular.module("resultsApp",['ui.bootstrap'])
                 return filtered;
             }
         };
-
         $scope.currentPage = 1;
         $scope.itemsPerPage = 3;
         $scope.maxSize = 2;
         $scope.totalItems = $scope.schools.length;
+    })
 
 
 
-
-
-
-
+    .filter('toArray', function () {
+        return function (obj, addKey) {
+            if (!angular.isObject(obj)) return obj;
+            if ( addKey === false ) {
+                return Object.keys(obj).map(function(key) {
+                    return obj[key];
+                });
+            } else {
+                return Object.keys(obj).map(function (key) {
+                    var value = obj[key];
+                    return angular.isObject(value) ?
+                        Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+                    { $key: key, $value: value };
+                });
+            }
+        };
     });
 
 
