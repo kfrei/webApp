fae3c39548b8f64782028199d939ddd04bda4f6e
kfrei <freiderikosk@hotmail.com>
2016-11-11 12:50:24 +0200

    .

---
+++ Schol.io/js/profile.js
@@ -0,0 +1,83 @@
+
+jQuery(document).ready(function($) {
+
+
+    //Sticky Menu
+
+
+
+
+    $(window).scroll(function(){
+
+        if($(window).scrollTop()>100){
+
+
+            $('tpb').removeClass("hidden-xs");
+            //$('.topbar').css({"display":"block"});
+
+
+        }else {
+            //$('.topbar').css({"display": "none"});
+            $('tpb').addClass("hidden-xs");
+        }
+    });
+
+
+
+    //Landing Page Menu Toggle - Collapse
+    var sandwitch = $('.sc-landing-menu-sandwitch-button');
+    //Οταν πατάς το sandwitch για να ανοίξει
+    sandwitch.click(function(){
+        console.log('ΠΑΤΗΘΗΚΕ');
+    });
+
+
+    //Landing Page Menu Toggle - Collapse
+    var itempressed = $('.sc-landing-menu-mobile-item-pressed');
+    var menuopen =   $('.sc-landing-menu-sandwitch');
+    var menuclose =   $('.sc-landing-menu-mobile-close');
+    var mobilemenu =$('.sc-landing-menu-mobile-holder');
+
+    //Οταν θέλω να ανοιξω το μενου
+
+    menuopen.click(function(){
+        mobilemenu.css({"display":"block"});
+    });
+
+    //Οταν θέλω να κλείσω το μενου
+
+    menuclose.click(function(){
+        mobilemenu.css({"display":"none"});
+    });
+
+    //Οταν θέλω να πατησω σε ενα αντικειμενο του μενου
+
+    itempressed.click(function(){
+        mobilemenu.css({"display":"none"});
+    });
+
+
+    //Smooth Scroll
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+
+
+
+
+
+
+});
+
