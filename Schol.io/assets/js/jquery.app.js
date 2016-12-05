f2ec5b3f8b90e453f423aae4fc80d42f13121f21
kfrei <freiderikosk@hotmail.com>
2016-12-05 11:42:10 +0200

    .

--- Schol.io/assets/js/jquery.app.js
+++ Schol.io/assets/js/jquery.app.js
@@ -294,13 +294,16 @@ function toggle_slimscroll(item){
     }
 }
 
-var wow = new WOW(
-  {
-    boxClass: 'wow', // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset: 50, // distance to the element when triggering the animation (default is 0)
-    mobile: false        // trigger animations on mobile devices (true is default)
-  }
-);
-wow.init();
+
+// ===============WOW=======================
+
+//var wow = new WOW(
+//  {
+//    boxClass: 'wow', // animated element css class (default is wow)
+//    animateClass: 'animated', // animation css class (default is animated)
+//    offset: 50, // distance to the element when triggering the animation (default is 0)
+//    mobile: false        // trigger animations on mobile devices (true is default)
+//  }
+//);
+//wow.init();
 
