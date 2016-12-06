70a5675e2a6bd591f80dd4a2f822bb28774fe492
kfrei <freiderikosk@hotmail.com>
2016-12-06 13:54:30 +0200

    .

---
+++ Schol.io/js/steps-app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by kfrei on 5/12/16.
+ */
+
+
+
+
+
+angular.module("stepsApp",['ngAnimate'])
+
+    .controller("stepsCtrl",function ($timeout,$scope) {
+
+        $scope.step1Select = 'reduce'; // set the default sort type
+
+
+        $scope.init = function () {
+            console.log("initialize !");
+        };
+
+
+        $timeout(function(){
+            $scope.ready = true;
+        },120);
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
