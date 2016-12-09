f2dabc3f39497c9bdae35dafea22affeb1e53bc8
kfrei <freiderikosk@hotmail.com>
2016-12-09 17:53:49 +0200

    .

--- Schol.io/js/steps-app.js
+++ Schol.io/js/steps-app.js
@@ -11,7 +11,7 @@ angular.module("stepsApp",['ngAnimate'])
     .controller("stepsCtrl",function ($timeout,$scope) {
 
         $scope.step1Select = 'reduce'; // set the default sort type
-
+        $scope.step2Select = 'Διοίκηση Επιχειρήσεων'; // set the default sort type
 
         $scope.init = function () {
             console.log("initialize !");
