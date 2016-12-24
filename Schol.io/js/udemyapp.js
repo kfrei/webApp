3208b110a74146cf5f9a32abc35c54d4723fbaf8
kfrei <freiderikosk@hotmail.com>
2016-12-24 04:30:47 +0200

    .

---
+++ Schol.io/js/udemyapp.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kfrei on 22/12/16.
+ */
+
+new Vue ({
+el: '#appUdemy',
+data:{
+    playerHealth:100,
+    monsterHealth:100,
+    gameIsRunning: false
+}
+
+
+});
---
+++ Schol.io/js/vue-app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by kfrei on 17/12/16.
+ */
+
+
+Vue.component('', {
+    props: ['', ''],
+    template: '',
+    mounted: function () {
+
+    },
+    watch: {
+
+    }
+
+})
+
+var vm = new Vue({
+    el: '#el',
+    template: '',
+    data: {
+        selected: 0,
+        options: [
+            { id: 1, text: 'Hello' },
+            { id: 2, text: 'World' }
+        ]
+    }
+})
+
